990c1c1cd691526c045855afc8cbc4baf922580e
adamnpeace <adam.n.peace@gmail.com>
2018-12-05 14:41:03 +0000

    Add test class

---
+++ trafficmon/test/com/trafficmon/CongestionChargeSystemTest.java
@@ -0,0 +1,16 @@
+package com.trafficmon;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+
+public class CongestionChargeSystemTest {
+
+
+    @Test
+    public void vehicleEnteringZone() {
+        assertThat(1, is(1));
+    }
+}
